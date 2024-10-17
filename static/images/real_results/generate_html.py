24f54214e0b7112930d4c0d134b0978bcd57eb57
Jingwei Ma <jingweim@Jingweis-MacBook-Pro-2.local>
2024-10-17 14:04:02 -0700

    added registered video

--- static/images/real_results/generate_html.py
+++ static/images/real_results/generate_html.py
@@ -1,20 +1,21 @@
 import glob
 import os
 
-# # video gallery
-# fnames = sorted(glob.glob('/Users/jingweim/Documents/VidPanos/vidpanos/static/images/real_results/*input.mp4'))
-# for idx, in_fname in enumerate(fnames):
-#     in_fname = in_fname.replace('/Users/jingweim/Documents/VidPanos/vidpanos', '.')
-#     out_fname = in_fname.replace('input.mp4', 'ours_lumiere.mp4')
-#     thumb_fname = in_fname.replace('input.mp4', 'thumb.jpg')
-#     print(f'<div class="thumbnail active" data-video1="{in_fname}" data-video2="{out_fname}">')
-#     print(f'    <img src="{thumb_fname}" alt="Video {idx+1}">')
-#     print('</div>')
-
-# results carousel
-fnames = sorted(os.listdir('/Users/jingweim/Documents/VidPanos/vidpanos/static/images/flow_baselines'))
-for idx, fname in enumerate(fnames):
-    print(f'<div class="item item-{idx+1}" style="display: flex; flex-direction: column; align-items: center;">')
-    print(f'    <video src="./static/images/flow_baselines/{fname}" autoplay controls muted loop playsinline></video>')
+# video gallery
+fnames = sorted(glob.glob('/Users/jingweim/Documents/VidPanos/vidpanos.github.io/static/images/real_results/*input.mp4'))
+for idx, in_fname in enumerate(fnames):
+    in_fname = in_fname.replace('/Users/jingweim/Documents/VidPanos/vidpanos', '.')
+    registered_fname = in_fname.replace('input.mp4', 'input_registered.mp4')
+    out_fname = in_fname.replace('input.mp4', 'ours_lumiere.mp4')
+    thumb_fname = in_fname.replace('input.mp4', 'thumb.jpg')
+    print(f'<div class="thumbnail active" data-video1="{in_fname}" data-video2="{registered_fname}" data-video3="{out_fname}">')
+    print(f'    <img src="{thumb_fname}" alt="Video {idx+1}">')
     print('</div>')
+
+# # baseline carousel
+# fnames = sorted(os.listdir('/Users/jingweim/Documents/VidPanos/vidpanos/static/images/flow_baselines'))
+# for idx, fname in enumerate(fnames):
+#     print(f'<div class="item item-{idx+1}" style="display: flex; flex-direction: column; align-items: center;">')
+#     print(f'    <video src="./static/images/flow_baselines/{fname}" autoplay controls muted loop playsinline></video>')
+#     print('</div>')
     
